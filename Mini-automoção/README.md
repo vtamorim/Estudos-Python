3ea6bb1a44be9387d810c8c9a0e790156552ded0
Victor Miguel <vthero89@gmail.com>
2024-09-06 11:29:05 -0300

    Initial commit

---
+++ Mini-automoção/README.md
@@ -0,0 +1,2 @@
+# Automoçaõ de Tarefa(SUAP[Windows, Microsoft Edge])
+Uma atividade que eu fiz da Jornada Python 
---
+++ Mini-automoção/ahm.py
@@ -0,0 +1,15 @@
+import pyautogui
+pyautogui.PAUSE=2
+pyautogui.press('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+pyautogui.write("suap")
+pyautogui.press('enter')
+pyautogui.click(496,292)
+pyautogui.click(1805,67)
+pyautogui.write("Sei Nome de Usuário:")
+pyautogui.press('tab')
+pyautogui.write('Sua senha:')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('enter')
