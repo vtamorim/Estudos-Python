5b92fd44175cdb30d9586937bb04ea1afab1395a
Victor Miguel <vthero89@gmail.com>
2025-07-28 07:21:27 -0300

    Testando mais coisas do kivy

--- Projetos Simples/finanças/fin.py
+++ Projetos Simples/finanças/fin.py
@@ -1,10 +1,23 @@
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button,Label
 
 
 class slamano(App):
-    def build(self):
-        return Button(text= "eae pai")
-    
 
-slamano().run()
+    def build(self):
+        layout = GridLayout(cols=3)
+        layout.add_widget(Label(text="primeirooooo"))
+        layout.add_widget(Label(text="segundo '-' "))
+        layout.add_widget(Label(text="terceirinho ,0, "))
+        self.mensagem = Label(text="")
+        layout.add_widget(Button(text="parapapa 4 ",on_press = self.qualquercoisa))
+        self.inputizin = TextInput(hint_text="alguma coisa kkkkk")
+        layout.add_widget(self.inputizin)
+        layout.add_widget(self.mensagem)
+        return layout
+    
+    def qualquercoisa(self,instancia):
+        self.mensagem.text = self.inputizin.text
+slamano().run() 
