a91d2b0e8d774257b3b9c5c9edab1d8f1ce2ba09
Victor Miguel <vthero89@gmail.com>
2026-04-07 14:39:49 -0300

    Teste com FastAPI

---
+++ Fast/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price : float
+    is_offer : bool | None = None
+
+
+
+@app.get("/")
+async def read_root():
+    return {"Nome": "Victor Miguel", "Curso": "Informática para Internet"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id : int, item : Item):
+    return {"item_name" : item.name, "item_id": item_id}
