854b5f03fd3002b0276c00417f76cc8acef2762e
Victor Miguel <vthero89@gmail.com>
2026-04-09 16:23:21 -0300

    Roteamento

--- Fast/routes/item_routes.py
+++ Fast/routes/item_routes.py
@@ -6,3 +6,12 @@ router = APIRouter(prefix="/items", tags=["Items"])
 items = []
 @router.post("/items/{item_id}")
 async def create_item(item_id: int, item: Item):
+    pass
+
+@router.get("/items")
+async def list_items():
+    pass
+
+@router.get("/items/{item_id}")
+async def get_item(item_id: int):
+    pass
